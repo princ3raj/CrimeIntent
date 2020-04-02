8b8e78155c7db22ed36d6eebcba96042ba87624d
princeraj <princ31999@gmail.com>
2020-04-02 16:15:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/crimeintent/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.crimeintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends SingleFragmentActivity {
+
+
+
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//       setContentView(R.layout.activity_fragment);
+//       // setContentView(R.layout.activity_fragment);
+//
+//        FragmentManager fm =
+//                getSupportFragmentManager();
+//
+//
+//        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+//        if (fragment == null) {
+//            fragment = new CrimeFragment();
+//            fm.beginTransaction()
+//                    .add(R.id.fragment_container,
+//                            fragment)
+//                    .commit();
+//        }
+//    }
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+
+}
